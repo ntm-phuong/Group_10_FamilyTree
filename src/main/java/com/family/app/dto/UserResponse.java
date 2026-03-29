32a26f39b63b5d7dbf140f2c90547e716e9f92f0
ntm-phuong <ntmphuonglao@gmail.com>
2026-03-29 21:12:57 +0700

    add api get, update profile , upload avatar

---
+++ src/main/java/com/family/app/dto/UserResponse.java
@@ -0,0 +1,21 @@
+package com.family.app.dto;
+
+import lombok.Data;
+import java.time.LocalDate;
+
+@Data
+public class UserResponse {
+    private String userId;
+    private String fullName;
+    private String email;
+    private String gender;
+    private LocalDate dob;
+    private String phoneNumber;
+    private String hometown;
+    private String currentAddress;
+    private String occupation;
+    private String bio;
+    private String avatar;
+    private String familyName;
+    private String roleName;
+}
